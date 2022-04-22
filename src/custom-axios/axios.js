71cf616295e87d25861cca6a93c778eb342a7bd7
Stefan <stefanbt998@outlook.com>
2022-04-23 00:07:24 +0200

    Fix

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: 'https://emt-lab02-backend.herokuapp.com/api',
     headers: {
-        "Access-Control-Allow-Headers" : "*"
+        "Access-Control-Allow-Origin" : "*"
     }
 });
 
